72959613d3df6acae768d2d3bae8ccea545cfd48
christiangubana <christiangubana@gmail.com>
2024-01-20 14:59:23 +0200

    update vite.config file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/api-data-display/",
+  // resolve: {
+  //   alias: {
+  //     "@": Path2D.resolve(__dirname, "./src"),
+  //   },
+  // },
 });
